e0a730748508a01f56c677c7eaaa937c3839b9b4
Michael Stroh <schleppi@michael-mbp.fritz.box>
2015-12-31 00:29:42 +0100

    Added two different sections. Added NavigationController. Added section titles.

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
+    
+    var textArraySection0 = ["Äpfel", "Birnen", "Zwetschgen"]
+    var textArraySection1 = ["Schnittlauch", "Petersilie"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,43 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var sectionTitle = ""
+        
+        if section == 0 {
+            sectionTitle = "Sektion Obst"
+        } else if section == 1 {
+            sectionTitle = "Sektion Gemüse"
+        }
+        return sectionTitle
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell!
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = textArraySection0[indexPath.row]
+        } else {
+            cell.textLabel?.text = textArraySection1[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var rowCount: Int = 0
+        if section == 0 {
+            rowCount = textArraySection0.count
+        } else if section == 1 {
+            rowCount = textArraySection1.count
+        }
+        return rowCount
     }
 
 
